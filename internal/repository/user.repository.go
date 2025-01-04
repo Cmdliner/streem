3607f85b22f2d499484bf3e2538ceb138267ba0e
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2025-01-04 23:41:46 +0100

    Feature: Impl user repository

--- internal/repository/user.repository.go
+++ internal/repository/user.repository.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"time"
 
-	"github.com/Cmdliner/streem/internal/model"
+	model "github.com/Cmdliner/streem/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -12,24 +14,82 @@ type UserRepository struct {
 	collection *mongo.Collection
 }
 
-func NewUserRepository(db *mongo.Database) (*UserRepository) {
+func NewUserRepository(db *mongo.Database) *UserRepository {
 	return &UserRepository{
 		collection: db.Collection("users"),
 	}
 }
 
 func (r *UserRepository) Create(user *model.User) (*model.User, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 10)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	user, err := r.collection.InsertOne(ctx, user)
-	if err != nil  {
+	result, err := r.collection.InsertOne(ctx, user)
+	if err != nil {
 		return nil, err
 	}
 
-	user.ID
+	user.ID = result.InsertedID.(primitive.ObjectID)
+
+	return user, nil
 }
 
-func (r * UserRepository) Update() {}
+func (r *UserRepository) GetById(id string) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
 
-func (r * UserRepository) Delete() {}
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var user model.User
+	err = r.collection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) GetByUsername(username string) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 10)
+	defer cancel()
+
+	var user model.User
+	err := r.collection.FindOne(ctx, bson.M{"username": username}).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) Update(id string, user *model.User) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	user.UpdatedAt = time.Now()
+
+	_, err = r.collection.UpdateOne(ctx, bson.M{"_id": objectID}, bson.M{"$set": user})
+	if err != nil {
+		return nil, err
+	}
+	return r.GetById(id)
+}
+
+func (r *UserRepository) Delete(id string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 10)
+	defer cancel()
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.collection.DeleteOne(ctx, bson.M{"_id": objectID})
+	return err
+}
