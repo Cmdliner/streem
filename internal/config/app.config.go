a1b388c625c5603fa60fba9e3dedd4eb89d466c8
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2025-01-04 21:44:38 +0100

    Add app config

---
+++ internal/config/app.config.go
@@ -0,0 +1,36 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Server struct {
+		Port string
+	}
+	MongoDB struct {
+		URI string
+		Name string
+	}
+	JWT struct {
+		Secret string
+		ExpirationHours int
+	}
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+
+}
